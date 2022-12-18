60576c8a36e5130837c9d7a04dc89f1c0aa8074a
QodirovDoston <qodirovdoston806@gmail.com>
2022-12-18 13:12:19 +0500

    djdkhsjc

--- indwex.js
+++ indwex.js
@@ -2,5 +2,30 @@
 // let  d= arr.fil
 
 
-let a ="2344"
-console.log(parseInt(a))
+// let a ="2344"
+// console.log(parseInt(a))
+
+
+// function frames(minutes, fps) {
+// 	return fps * (minutes * 60)
+// }
+// console.log(frames(10, 10));
+
+
+// function maxNum(n1,n2) {
+// 	if (n1 < n2) {
+// 	console.log( n2);
+// 	}
+//   else {
+// 	console.log(n1); 
+//   }
+// }
+// maxNum(90,7)
+function maxNum(n1,n2) {
+	if (n1 < n2  ) {
+        console.log(n2); 
+    } else {
+    console.log(n1); 
+  }
+}
+maxNum(93,7)
