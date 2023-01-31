62067ad25ff85d6d8864b3c737d3d542d56fab56
QodirovDoston <qodirovdoston806@gmail.com>
2023-01-31 18:18:14 +0500

    sdsgc

--- 2022-yil-December/indwex.js
+++ 2022-yil-December/indwex.js
@@ -160,16 +160,12 @@
 
 
 
-// function minMax(arr) {
-// 	return arr.Math.min(), arr.Math.max()
-// }
-// console.log(minMax([1, 2, 3, 4, 5]))
 
 // class user {
-// 	 Person(name, age)	
-// }
-// Person("doston",23)
-// console.log(`salom mening ismim ${name}`);
+	// 	 Person(name, age)	
+	// }
+	// Person("doston",23)
+	// console.log(`salom mening ismim ${name}`);
 
 // function addition(num) {
 // 	return ++num
@@ -177,11 +173,203 @@
 // console.log(addition(2));
 
 
-let a = [10, 24, 3, 5, 7, 8, 9,]
-let min = arr[0]
-for (let i = 0; i <= arr.length; i++) {
-	if (arr[i] < min) {
-		min = arr[i];
+// let a = [10, 24, 3, 5, 7, 8, 9,]
+// let min = arr[0]
+// for (let i = 0; i <= arr.length; i++) {
+	// 	if (arr[i] < min) {
+		// 		min = arr[i];
+		// 	}
+		// }
+		// console.log(min);
+		
+		// function isFirstSuperior(arr1, arr2) {
+			// 	return arr1 > arr2
+			// 	}
+			//     console.log(isFirstSuperior([1,3,4],[1,3,4]));
+			
+			// function char_count(str, letter) 
+			// {
+				//  var letCount = 0;
+				//  for (var position = 0; position < str.length; position++) 
+				//  {
+					//     if (str.charAt(position) == letter) 
+//       {
+//       letCount += 1;
+//       }
+//   }
+//   return letCount;
+// }
+// console.log(char_count('doston beol ', 'd'));
+
+
+// function charCount(myChar, str) {
+	//     var result = 0;
+	//     for(var i = 0; i < myChar.length; i++) {
+		//     if(myChar[i] === str) {
+			//     result++;
+			//     }
+			//     }
+			//     return result
+			//     }
+			//     console.log(charCount('ssssssssssJsssssssssssssjwasigbsxssssssssssss', 'j'));
+			
+			
+			// function charCount(myChar, str) {
+				//     var result = 0;
+				//       for(var i = 0; i < str.length; i++) {
+					//      if(str[i] === myChar) {
+						//     result++;
+						//     }
+						//     }
+						//     return result;
+						//     };
+						//    console.log(charCount('ssssssssssJss', 's'));
+						
+						
+						// function mubashirFunction(a, b) {
+							// 	const left = ("" +a).split('').reduce((acc,cv) => acc + +cv,0)
+							// 	const right = ("" +b).split('').reduce((acc,cv) => acc + +cv,0) 
+							// 	return  left * right  
+							// }
+							// console.log(mubashirFunction(100, 100))
+							/////////////////////////////////////////
+							
+							
+							// for of ni ishlatish 
+							
+							// let aarr=[1,23,4,56,7,8,9,9,]
+							// for(element of aarr){
+								// 	console.log(element);
+								// }
+								
+								
+								//  let asd = {
+									// 	name:"doton",
+									// 	age:33,
+									// 	hight:189,
+									// 	year:2000,
+									//  }
+									//  for( ad in asd){
+										// 	console.log(ad,asd[ad]);
+										//  }
+										
+										
+										
+										// function isBigEnough(value) {
+											// 	return value >= 10;
+											//   }
+  
+//   const filtered = [12, 5, 8, 130, 44].filter(isBigEnough);
+//   console.log(filtered);
+
+
+// function filterArray(arr) {
+	// 	var number = arr.filter(function (a) {
+		// 	  if (typeof a == 'number' || a === 0)
+		// 		return a;
+		// 	})
+		// 	return number
+		//   }
+		//   console.log(filterArray([1,3,4,5,6,8,9,0,"dd"]) );
+		
+		
+		// function doubleChar(str) {
+		// 		return str.split('').map(v => v.repeat(2)).join('');
+		// 		}
+		// 		console.log(doubleChar("Doston"));
+			
+
+
+			
+			// let str = "+7-123-45-6783272 8472393";
+			
+			// let numbers = str.match(/\d{2,}/g);
+			
+			// console.log(numbers.join("-"));
+
+
+				// const array =(a)=>{
+				// 	return a.reduce((a, b) => Math.max(a, b), -Infinity)
+				// }
+				// console.log(array([1,36,8,90]));
+
+
+
+// const arr = [3,4,5,3,6, 7, 8];
+// const count = {};
+// for (const element of arr) {
+//   count[element] = (count[element] || 0) + 1;
+// }
+// console.log(count);
+
+
+// let a ="solaom"
+// for (let i = 0; i < a.length; i++) {
+// 	const element = a[i];
+// }
+// if(a.length === [a,u,i,e,o]){
+// 	return i
+// }else return 0
+
+
+
+// function vowel(str){
+// var list = 'e E';
+// var v = 0;
+// for(var i = 0; i < str.length ; i++)
+// {
+// if (list.indexOf(str[i]) !== -1)
+// {
+// v += 1;
+// }
+// }
+// return v;
+// }
+// console.log(vowel("dAosteon"));
+
+
+// function stringE(a) {
+//     let list = "e"
+//     let count=0
+//     for(let i=0; i<=a.length; i++){
+//        if(list.indexOf(a[i]) !== -1){count++}
+//     }
+//     return count <=3 && count ? true : false
+// }
+// console.log(stringE("Hello"));
+
+
+// const vowels = ["a","e","i","o","u"]
+// function countVowel(str) {
+//     let count = 0;
+//     for (let letter of str.toLowerCase()) {
+//         if (vowels.includes(letter)) {
+//             count++;
+//         }
+//     }
+//     return count
+// }console.log(countVowel('AbceiodefGhi'));
+
+
+// function error(n){
+// 	let errors = 
+// 	{
+// 	  1: "Check the fan: e1",
+// 	  2: "Emergency stop: e2",
+// 	  3: "Pump Error: e3",
+// 	  4: "c: e4",
+// 	  5: "Temperature Sensor Error: e5"
+// 	}
+// 	console.log(Object.values(errors).find(val=> val.includes(n)))
+// 	}
+// 	error(2)
+
+function largestSwap(num) {
+	let num1 = num.reverse()
+	if( num1 < num ){
+		return true;
+	}else (num1 > num){
+		return false;
 	}
 }
-console.log(min);
+console.log(largestSwap(23));
